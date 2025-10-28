bf761717dd1f64d136c016ad8482182640bb4b95
YuJin Song <syujin2323@gmail.com>
2025-10-28 14:08:15 +0900

    과제 제출

--- bird.h
+++ bird.h
@@ -28,3 +28,49 @@ public:
         std::cout << species << " has died" << std::endl;
      }
 };
+
+class Duck : public Bird {
+
+public:
+    Duck() : Bird("Duck") { cry(); }
+
+    ~Duck() override { cry(); }
+
+    void cry() override {
+        std::cout << "Quack!" << std::endl;
+    }
+
+};
+
+class Penguin : public Bird {
+
+public:
+    Penguin() : Bird("Penguin") { cry(); }
+
+    ~Penguin() override { cry(); }
+
+    void fly() override {
+        std::cout << "This bird can't fly." << std::endl;
+    }
+
+    void cry() override {
+        std::cout << "Squawk!" << std::endl;
+    }
+
+};
+
+class Eagle : public Bird {
+public:
+    Eagle() : Bird("Eagle") { cry(); }
+
+    ~Eagle() override { cry(); }
+
+    void swim() override {
+        std::cout << "This bird can't swim." << std::endl;
+    }
+
+    void cry() override {
+        std::cout << "Screech!" << std::endl;
+    }
+
+};
